9f233e8e519cab29a1920cb92b1992c45d1d1e71
lecheng <lechenggood@foxmail.com>
2024-04-08 15:19:22 +0800

    更新部分引脚
    Signed-off-by: lecheng <lechenggood@foxmail.com>

--- MemoryLFO/MemoryLFO_for_Lgt8f328p/MemoryLFO_for_Lgt8f328p.ino
+++ MemoryLFO/MemoryLFO_for_Lgt8f328p/MemoryLFO_for_Lgt8f328p.ino
@@ -1,5 +1,6 @@
 //记忆旋钮电压模块
 #define KNOB_PIN 3   //旋钮引脚
+#define RST_PIN 4    //复位引脚
 #define BTN1_PIN 12  //按钮引脚
 #define BTN2_PIN 13  //按钮引脚
 #define OUTA_PIN 11  //OUT PWM引脚
@@ -8,10 +9,12 @@ int value[768] = { 0 };  //给部分数组元素赋值
 int position = 0;        //最大值为768
 int lastBtn1 = 1;
 int memStep = 768;
+byte clockLv2 = 0;
 
 void setup() {
   pinMode(BTN1_PIN, INPUT_PULLUP);
   pinMode(BTN2_PIN, INPUT_PULLUP);
+  pinMode(RST_PIN, INPUT);
 
   Serial.begin(115200);  //使用Serial.begin()函数来初始化串口波特率,参数为要设置的波特率
 }
@@ -43,7 +46,20 @@ void loop() {
   }
 
   Serial.print(" position= ");
-  Serial.print(position);
+  Serial.println(position);
+  Serial.print(" d4= ");
+  Serial.print(digitalRead(4));
+
+  //gate信号触发复位
+  if (digitalRead(4) != clockLv2 && clockLv2 == 0) {
+    position = 0;
+    // Serial.println("Rst");
+    clockLv2 = 1;
+  }
+  //暂存复位触发信息
+  if (digitalRead(4) != clockLv2 && clockLv2 == 1) {
+    clockLv2 = 0;
+  }
   // Serial.print(" value= ");
   // Serial.print(value[position]);
   int tempPosition = position < 1 ? 0 : position - 1;
