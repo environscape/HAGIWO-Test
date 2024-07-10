0d0e230f7de8880347042dda95a3f876d257c2ba
lecheng <lechenggood@foxmail.com>
2024-07-10 17:33:05 +0800

    增加总控 分频率参数
    Signed-off-by: lecheng <lechenggood@foxmail.com>

--- MainLFO/MainLFO.ino
+++ MainLFO/MainLFO.ino
@@ -10,6 +10,9 @@ const int PIN_NUM = 6;
 const int CV_PIN[PIN_NUM] = { 3, 5, 6, 9, 10, 11 };
 float cv[PIN_NUM] = { 0, 0, 0, 0, 0, 0 };
 float coef[PIN_NUM] = { 1, 0.73, 0.57, 0.41, 0.29, 0.13 };
+double wave_position[PIN_NUM] = {};
+double lfo_length = 16384;
+int lfo_type = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -24,15 +27,27 @@ void setup() {
 }
 
 bool tmp_btn = 0;
+bool tmp_clk = 0;
 
 void loop() {
   //read btn
   if (digitalRead(BUTTON_PIN) == 0 && tmp_btn == 1) {
     tmp_btn = 0;
+    lfo_type++;
+    if (lfo_type > 7) lfo_type = 0;
   }
   if (digitalRead(BUTTON_PIN) == 1 && tmp_btn == 0) {
     tmp_btn = 1;
   }
+  //read clk
+  if (digitalRead(CLK_IN) == 1 && tmp_clk == 0) {
+    tmp_clk = 1;
+    for (int i = 0; i < PIN_NUM; i++) wave_position[i] = 0;
+  }
+  if (digitalRead(CLK_IN) == 0 && tmp_clk == 1) {
+    tmp_clk = 0;
+  }
+
 
   //view knob value
   if (!digitalRead(BUTTON_PIN)) {
@@ -41,21 +56,66 @@ void loop() {
   }
 
 
-  float knob_coef = (4096 - analogRead(KNOB_PIN)) / 1024;
-  //SAW UP
+  float knob_coef = (4096 - analogRead(KNOB_PIN)) / 128;
   for (int i = 0; i < PIN_NUM; i++) {
-    cv[i] += 0.2 * coef[i] * knob_coef;
-    if (cv[i] > 255) cv[i] = 0;
+    if (wave_position[i] > lfo_length) wave_position[i] = 0;  //2^13
+    wave_position[i] += knob_coef * coef[i];
+    // wave select
+    switch (lfo_type) {
+      default:  //Sin
+        if (wave_position[i] < lfo_length / 2)
+          cv[i] = (1 - (wave_position[i] / lfo_length * 2)) * 255;
+        else
+          cv[i] = (wave_position[i] / lfo_length * 2) * 255;
+        break;
+      case 1:  //Pluse 1
+        if (wave_position[i] < lfo_length / 4)
+          cv[i] = 255;
+        else
+          cv[i] = 0;
+        break;
+      case 2:  //PLUSE 2
+        if (wave_position[i] < lfo_length / 2)
+          cv[i] = 255;
+        else
+          cv[i] = 0;
+        break;
+      case 3:  //PLUSE 3
+        if (wave_position[i] > lfo_length / 4)
+          cv[i] = 255;
+        else
+          cv[i] = 0;
+        break;
+      case 4:  //SAW DOWN
+        cv[i] = (1 - (wave_position[i] / lfo_length)) * 255;
+        break;
+      case 5:
+      case 6:  //SAW UP 256=2^8
+        cv[i] = (wave_position[i] / lfo_length) * 255;
+        break;
+      case 7:  //Random
+        cv[i] = random(0, 255);
+        break;
+    }
   }
+
 
   for (int i = 0; i < PIN_NUM; i++)
     analogWrite(CV_PIN[i], cv[i]);
 
 
   Serial.print(" a1=");
-  Serial.print(analogRead(KNOB_PIN));
+  Serial.print((4096 - analogRead(KNOB_PIN)));
+  Serial.print(" knob_coef=");
+  Serial.print(knob_coef);
   Serial.print(" d7=");
   Serial.print(digitalRead(BUTTON_PIN));
+  Serial.print(" lfo_type=");
+  Serial.print(lfo_type);
+  Serial.print(" p=");
+  Serial.print(wave_position[0]);
+  Serial.print(" cv=");
+  Serial.print(cv[0]);
   Serial.print(" \n");
 }
 
